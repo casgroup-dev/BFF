a59ec9476aaaa3bfdf072785a898261c9dce2477
Daniela Ruz <druzmieres@gmail.com>
2018-04-04 14:58:10 -0300

    adds some error labels

--- src/components/GeneralViews/register/Form.vue
+++ src/components/GeneralViews/register/Form.vue
@@ -5,20 +5,23 @@
     </div>
     <div class="block">
       <label>Nombre</label>
-      <fg-input class="col-12" placeholder="Juan Pérez González" @enter="login"/>
+      <fg-input class="col-12" placeholder="Juan Pérez González" v-model="name.payload" @enter="login"/>
     </div>
     <div class="block">
       <label>Correo electrónico</label>
-      <fg-input class="col-12" placeholder="ejemplo@ejemplo.com" @enter="login"/>
+      <label class="error" v-if="email.error">Necesitas ingresar un email</label>
+      <fg-input class="col-12" placeholder="ejemplo@ejemplo.com" v-model="email.payload" @enter="login"/>
     </div>
     <div class="block">
       <label>Empresa</label>
-      <fg-input class="col-12" placeholder="CasGroup" @enter="login"/>
+      <label class="error" v-if="company.error">Necesitas ingresar el nombre de tu empresa</label>
+      <fg-input class="col-12" placeholder="CasGroup" v-model="company.payload" @enter="login"/>
     </div>
     <div class="block">
       <!-- TODO change dropdown menu style -->
       <label>Rol</label>
-      <select class="col-12" v-model="selected">
+      <label class="error" v-if="role.error">Necesitas ingresar tu rol</label>
+      <select class="col-12" v-model="role.payload">
         <option disabled value="">Por favor elija uno</option>
         <option>Administrador</option>
         <option>Proveedor</option>
@@ -27,16 +30,22 @@
     </div>
     <div class="block">
       <label>Teléfono</label>
-      <fg-input class="col-12" placeholder="+56 9 1234 5678" @enter="login"/>
+      <fg-input class="col-12" placeholder="+56 9 1234 5678" v-model="phone.payload" @enter="login"/>
     </div>
     <div class="block">
       <label>Contraseña</label>
-      <fg-input class="col-12" placeholder="****" @enter="login"/>
+      <label class="error" v-if="password.error">Necesitas ingresar una contraseña</label>
+      <fg-input class="col-12" placeholder="******" type="password" v-model="password.payload" @enter="login"/>
+    </div>
+    <div class="block">
+      <!-- TODO check matching passwords -->
+      <label>Confirmar contraseña</label>
+      <label class="error" v-if="passwordConfirm.error">Debes confirmar la contraseña</label>
+      <fg-input class="col-12" placeholder="******" type="password" v-model="passwordConfirm.payload" @enter="login"/>
     </div>
     <div align="center">
       <button class="btn btn-info btn-fill" @click="login" v-if="!loading">Registrar</button>
     </div>
-
 
     <p>¿Qué deberían hacer?</p>
     <ul>
@@ -56,20 +65,32 @@
 <script>
   import Card from '../../UIComponents/Cards/Card'
   import usersApi from '../../../apis/users'
+  import ClipLoader from 'vue-spinner/src/ClipLoader'
 
   export default {
     name: 'form',
-    components: {
-      Card
-    },
     data () {
-      return {}
+      return {
+        name: {payload: null, error: false},
+        email: {payload: null, error: false},
+        company: {payload: null, error: false},
+        role: {payload: null, error: false},
+        phone: {payload: null, error: false},
+        password: {payload: null, error: false},
+        passwordConfirm: {payload: null, error: false},
+        loading: false,
+        loginErrorMessage: null
+      }
+    },
+    components: {
+      Card,
+      ClipLoader
     },
     methods: {
       /**
        * Check that the data of the form is filled (not empty values) and call the register API.
        */
-      register () {
+      async register () {
         throw new Error('Not implemented.')
       }
     }
