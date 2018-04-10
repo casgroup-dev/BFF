e6e8717da0c065d7e7370ea0f46cddf327770cbd
gyahu <tpvchile@gmail.com>
2018-04-10 11:14:20 -0300

    Removes role attribute and alignes boxes

--- src/components/GeneralViews/register/Form.vue
+++ src/components/GeneralViews/register/Form.vue
@@ -6,45 +6,34 @@
     <label class="error" v-if="registerErrorMessage">{{registerErrorMessage}}</label>
     <label align="center-block" class="error" v-if="name.error">Ingrese su nombre</label>
     <div class="block">
-      <label>Nombre*</label>
-      <fg-input class="col-12" placeholder="Juan Pérez González" v-model="name.payload" @enter="register"/>
+      <fg-input class="col-12" placeholder="Juan Pérez González" v-model="name.payload" @enter="register">
+        <label slot="label">Nombre</label></fg-input>
     </div>
     <label align="center-block" class="error" v-if="email.error">Ingrese una dirección de correo</label>
     <div class="block">
       <!-- TODO check email format -->
-      <label>Correo electrónico*</label>
-      <fg-input class="col-12" placeholder="ejemplo@ejemplo.com" type="email" v-model="email.payload" @enter="register"/>
+      <fg-input class="col-12" placeholder="ejemplo@ejemplo.com" type="email" v-model="email.payload" @enter="register">
+        <label slot="label">Correo electrónico</label></fg-input>
     </div>
     <label class="error" v-if="company.error">Ingrese el nombre de su empresa</label>
     <div class="form-group">
-      <label>Empresa*</label>
-      <fg-input class="col-12" placeholder="CasGroup" v-model="company.payload" @enter="register"/>
-    </div>
-    <label class="error" v-if="role.error">Ingrese su rol</label>
-    <div class="block">
-      <!-- TODO change dropdown menu style -->
-      <label>Rol*</label>
-      <select class="col-12" v-model="role.payload">
-        <option disabled value="">Por favor elija uno</option>
-        <option>Administrador</option>
-        <option>Proveedor</option>
-        <option>Consultor</option>
-      </select>
+      <fg-input class="col-12" placeholder="CasGroup" v-model="company.payload" @enter="register">
+        <label slot="label">Empresa</label></fg-input>
     </div>
     <div class="block">
-      <label>Teléfono</label>
-      <fg-input class="col-12" placeholder="+56 9 1234 5678" v-model="phone.payload" @enter="register"/>
+      <fg-input class="col-12" placeholder="+56 9 1234 5678" v-model="phone.payload" @enter="register">
+        <label slot="label">Teléfono</label></fg-input>
     </div>
     <label class="error" v-if="password.error">Ingrese una contraseña</label>
     <div class="block">
-      <label>Contraseña*</label>
-      <fg-input class="col-12" placeholder="******" type="password" v-model="password.payload" @enter="register"/>
+      <fg-input class="col-12" placeholder="******" type="password" v-model="password.payload" @enter="register">
+        <label slot="label">Contraseña</label></fg-input>
     </div>
     <label class="error" v-if="passwordConfirm.error">Confirme la contraseña</label>
     <div class="block">
       <!-- TODO check matching passwords -->
-      <label>Confirmar contraseña*</label>
-      <fg-input class="col-12" placeholder="******" type="password" v-model="passwordConfirm.payload" @enter="register"/>
+      <fg-input class="col-12" placeholder="******" type="password" v-model="passwordConfirm.payload" @enter="register">
+        <label slot="label">Confirmar contraseña</label></fg-input>
     </div>
     <label class="error" v-if="differentPasswordsError">Contraseñas no coinciden</label>
     <div align="center">
@@ -74,7 +63,6 @@
         name: {payload: null, error: false},
         email: {payload: null, error: false},
         company: {payload: null, error: false},
-        role: {payload: null, error: false},
         phone: {payload: null, error: false},
         password: {payload: null, error: false},
         passwordConfirm: {payload: null, error: false},
@@ -98,50 +86,20 @@
         if (!this.name.payload){ // TODO Obligatorio?
           this.name.error = true
         }
-        else {
-          this.name.error = false
-        }
         if (!this.email.payload){
           this.email.error = true
-          }
-        else {
-          this.email.error = false
         }
         if (!this.company.payload){
           this.company.error = true
         }
-        else{
-          this.company.error = false
-        }
-        if (!this.role.payload){  // TODO Asignado o decidido?
-          this.role.error = true
-        }
-        else{
-          this.role.error = false
-        }
         if (!this.phone.payload){ // TODO Obligatorio?
           this.phone.error = true
-        }
-        else{
-          this.phone.error = false
         }
         if (!this.password.payload) {
           this.password.error = true
         }
-        else{
-          this.password.error = false
-        }
         if (!this.passwordConfirm.payload) {
           this.passwordConfirm.error = true
-        }
-        else{
-          this.passwordConfirm.error = false
-        }
-        if (this.password.payload && this.passwordConfirm.payload && !(this.passwordConfirm.payload === this.password.payload)){
-          this.differentPasswordsError = true
-        }
-        else{
-          this.differentPasswordsError = false
         }
         if (this.name.payload && this.email.payload && this.company.payload && this.password.payload && this.passwordConfirm.payload &&
           (this.passwordConfirm.payload === this.password.payload)) {
