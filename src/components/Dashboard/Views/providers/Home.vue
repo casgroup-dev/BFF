dc34c5be2686bb6ed20fd1e40b9072f33b9e10a4
Sebastián Pujá López <sebastian.puja.lopez@gmail.com>
2018-04-17 15:20:31 -0300

    Refactors header of providers view

--- src/components/Dashboard/Views/providers/Home.vue
+++ src/components/Dashboard/Views/providers/Home.vue
@@ -5,74 +5,75 @@
         <div class="col-12">
           <card>
             <template slot="header">
-              <h4 class="card-title">Proveedores
-                <router-link :to="{ name: 'Nueva', query: {next: this.$route.query.next}}">
-                  <a style="float: right; width: 250px"><i class="nc-icon nc-simple-add"></i></a>
-                </router-link>
-              </h4>
-              <p class="card-category">Actuales proveedores disponibles para CasGroup
-                <router-link :to="{ name: 'Nueva', query: {next: this.$route.query.next}}">
-                  <a style="float: right; width: 250px">Agregar Proveedor</a>
-                </router-link>
-              </p>
+              <div class="row">
+                <div class="offset-3 col-6" style="text-align: center; font-size: xx-large">
+                  Proveedores
+                </div>
+                <div class="col-3" style="text-align: right; font-size: xx-large">
+                  <router-link :to="{ name: 'Nueva', query: {next: this.$route.query.next}}">
+                    <button class="btn btn-primary" style="font-size: large">Agregar proveedor</button>
+                  </router-link>
+                </div>
+              </div>
             </template>
             <template>
               <div class="row">
-                <fg-input class="col-6" v-model="search" placeholder="Proveedor" addon-right-icon="nc-icon nc-zoom-split">
+                <fg-input class="col-6" v-model="search" placeholder="Proveedor"
+                          addon-right-icon="nc-icon nc-zoom-split">
                 </fg-input>
               </div>
             </template>
             <template>
-            <div class="table-responsive">
-              <table class="table table-hover table-striped">
-                <thead>
-                <th v-for="attr in table.columns">
-                  <tr scope="col">{{attr}}</tr>
-                </th>
-                </thead>
-                <tbody>
-                <template v-for="licit in filteredProviders">
-                  <tr>
-                    <td v-for="attr in licit.attributes">
-                      <a
-                        style="font-weight:normal; color:#262626;"
+              <div class="table-responsive">
+                <table class="table table-hover table-striped">
+                  <thead>
+                  <th v-for="attr in table.columns">
+                    <tr scope="col">{{attr}}</tr>
+                  </th>
+                  </thead>
+                  <tbody>
+                  <template v-for="licit in filteredProviders">
+                    <tr>
+                      <td v-for="attr in licit.attributes">
+                        <a
+                          style="font-weight:normal; color:#262626;"
+                          data-toggle="collapse"
+                          role="button"
+                          v-on:click="licit.show = !licit.show">
+                          {{attr}}
+                        </a>
+                      </td>
+                      <td><a
+                        style="color:#262626;"
                         data-toggle="collapse"
                         role="button"
                         v-on:click="licit.show = !licit.show">
-                        {{attr}}
-                      </a>
-                    </td>
-                    <td><a
-                      style="color:#262626;"
-                      data-toggle="collapse"
-                      role="button"
-                      v-on:click="licit.show = !licit.show">
-                      <i class="nc-icon nc-stre-down" v-if="!licit.show"></i>
-                      <i class="nc-icon nc-stre-up" v-else></i>
-                    </a></td>
-                  </tr>
-                  <transition name="fade" mode="out-in" appear>
-                    <tr>
-                      <td v-if="licit.show"> <!-- TODO Componentes de una Licitacion -->
-                        <tr>Cronograma</tr> <!-- TODO Componente Propio -->
-                        <tr>Bases</tr> <!-- TODO Componente Propio -->
-                        <tr>Estado Licitación</tr>
-                        <tr>Subir Documentos</tr> <!-- TODO Componente Propio -->
-                        <tr>Preguntas/Respuestas</tr> <!-- TODO Componente Propio -->
-                        <tr>Evaluaciones (Técnico, Comercial, Economico)</tr> <!-- TODO Componente Propio -->
-                        <tr>Cuadro Comparativo</tr> <!-- TODO Componente Propio -->
-                      </td>
+                        <i class="nc-icon nc-stre-down" v-if="!licit.show"></i>
+                        <i class="nc-icon nc-stre-up" v-else></i>
+                      </a></td>
                     </tr>
-                  </transition>
-                </template>
-                </tbody>
-              </table>
-              <div class="row">
-                <div class="col-12" style="text-align: center">
-                  <button class="btn btn-center btn-info btn-round">Cargar más</button>
+                    <transition name="fade" mode="out-in" appear>
+                      <tr>
+                        <td v-if="licit.show"> <!-- TODO Componentes de una Licitacion -->
+                      <tr>Cronograma</tr> <!-- TODO Componente Propio -->
+                      <tr>Bases</tr> <!-- TODO Componente Propio -->
+                      <tr>Estado Licitación</tr>
+                      <tr>Subir Documentos</tr> <!-- TODO Componente Propio -->
+                      <tr>Preguntas/Respuestas</tr> <!-- TODO Componente Propio -->
+                      <tr>Evaluaciones (Técnico, Comercial, Economico)</tr> <!-- TODO Componente Propio -->
+                      <tr>Cuadro Comparativo</tr> <!-- TODO Componente Propio -->
+                      </td>
+                      </tr>
+                    </transition>
+                  </template>
+                  </tbody>
+                </table>
+                <div class="row">
+                  <div class="col-12" style="text-align: center">
+                    <button class="btn btn-center btn-info btn-round">Cargar más</button>
+                  </div>
                 </div>
               </div>
-            </div>
             </template>
           </card>
         </div>
@@ -83,6 +84,7 @@
 <script>
   import LTable from 'src/components/UIComponents/Table.vue'
   import Card from 'src/components/UIComponents/Cards/Card.vue'
+  import bla from 'vue'
 
   const tableColumns = [
     'Nombre Fantasia',
@@ -151,9 +153,8 @@
       LTable,
       Card
     },
-    methods: {
-    },
-    data: function() {
+    methods: {},
+    data: function () {
       return {
         search: '',
         table: {
@@ -164,15 +165,22 @@
     },
     computed:
       {
-        filteredProviders:function()
-        {
-          var self=this;
-          return this.table.data.filter(function(cust){return cust.attributes.industria.toLowerCase().indexOf(self.search.toLowerCase())>=0;});
+        filteredProviders: function () {
+          var self = this;
+          return this.table.data.filter(function (cust) {
+            return cust.attributes.industria.toLowerCase().indexOf(self.search.toLowerCase()) >= 0;
+          });
           //return this.customers;
         }
       }
   }
+
 </script>
 <style scoped src="../../../../assets/css/animations/fade.css">
 </style>
-
+<style>
+  my_title {
+    text-align: center;
+    font-size: xx-large
+  }
+</style>
