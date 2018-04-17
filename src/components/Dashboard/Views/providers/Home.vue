9d202c68f1ae995b4663496b4f5d58e2a2e81af5
gyahu <tpvchile@gmail.com>
2018-04-17 10:19:40 -0300

    Adds alternative providers front to maintain the original template.

---
+++ src/components/Dashboard/Views/providers/Home.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <card>
+            <template slot="header">
+              <h4 class="card-title">Proveedores
+                <router-link :to="{ name: 'Nueva', query: {next: this.$route.query.next}}">
+                  <a style="float: right; width: 250px"><i class="nc-icon nc-simple-add"></i></a>
+                </router-link>
+              </h4>
+              <p class="card-category">Últimas licitaciones realizadas en la plataforma
+                <router-link :to="{ name: 'Nueva', query: {next: this.$route.query.next}}">
+                  <a style="float: right; width: 250px">Nueva Licitación</a>
+                </router-link>
+              </p>
+            </template>
+            <div class="table-responsive">
+              <table class="table table-hover table-striped">
+                <thead>
+                <th v-for="attr in table.columns">
+                  <tr scope="col">{{attr}}</tr>
+                </th>
+                </thead>
+                <tbody>
+                <template v-for="licit in table.data">
+                  <tr>
+                    <td v-for="attr in licit.attributes">
+                      <a
+                        style="font-weight:normal; color:#262626;"
+                        data-toggle="collapse"
+                        role="button"
+                        v-on:click="licit.show = !licit.show">
+                        {{attr}}
+                      </a>
+                    </td>
+                    <td><a
+                      style="color:#262626;"
+                      data-toggle="collapse"
+                      role="button"
+                      v-on:click="licit.show = !licit.show">
+                      <i class="nc-icon nc-stre-down" v-if="!licit.show"></i>
+                      <i class="nc-icon nc-stre-up" v-else></i>
+                    </a></td>
+                  </tr>
+                  <transition name="slide" :duration="500">
+                    <tr>
+                      <td v-if="licit.show"> <!-- TODO Componentes de una Licitacion -->
+                    <tr>Cronograma</tr> <!-- TODO Componente Propio -->
+                    <tr>Bases</tr> <!-- TODO Componente Propio -->
+                    <tr>Estado Licitación</tr>
+                    <tr>Subir Documentos</tr> <!-- TODO Componente Propio -->
+                    <tr>Preguntas/Respuestas</tr> <!-- TODO Componente Propio -->
+                    <tr>Evaluaciones (Técnico, Comercial, Economico)</tr> <!-- TODO Componente Propio -->
+                    <tr>Cuadro Comparativo</tr> <!-- TODO Componente Propio -->
+                    </td>
+                    </tr>
+                  </transition>
+                </template>
+                </tbody>
+              </table>
+              <div class="row">
+                <div class="col-12" style="text-align: center">
+                  <button class="btn btn-center btn-info btn-round">Cargar más</button>
+                </div>
+              </div>
+            </div>
+          </card>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  import LTable from 'src/components/UIComponents/Table.vue'
+  import Card from 'src/components/UIComponents/Cards/Card.vue'
+
+  const tableColumns = ['Nombre', 'Fecha creación', 'Última edición', 'Estado', 'Etapa']
+  const tableData = Array(13).fill({
+    attributes: {
+      nombre: 'Snacks Copec de Rancagua',
+      'fecha creación': '20 de Marzo de 2018',
+      'última edición': '25 de Marzo de 2018',
+      estado: 'Cerrada',
+      etapa: 'Revisión',
+    },
+    show: false
+  })
+  export default {
+    components: {
+      LTable,
+      Card
+    },
+    methods: {
+    },
+    data () {
+      return {
+        table: {
+          columns: [...tableColumns],
+          data: [...tableData]
+        }
+      }
+    }
+  }
+</script>
+<style>
+</style>
+
