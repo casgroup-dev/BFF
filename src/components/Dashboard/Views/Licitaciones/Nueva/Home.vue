a0ad701b8f29cb7d56525453ef4a40fd85e4ef07
gyahu <tpvchile@gmail.com>
2018-04-19 12:57:03 -0300

    Adds a basic form for licitations

--- src/components/Dashboard/Views/Licitaciones/Nueva/Home.vue
+++ src/components/Dashboard/Views/Licitaciones/Nueva/Home.vue
@@ -1,20 +1,26 @@
 <template>
-  <form>
-    <div class="form-group">
-      <label for="exampleInputEmail1">Email address</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  <div class="content">
+    <div class="container-fluid">
+      <form>
+        <div class="form-group">
+          <label for="licitName">Nombre</label>
+          <input type="text" class="form-control" id="licitName" placeholder="Nombre Licitación">
+        </div>
+        <div class="form-group">
+          <label for="empresaAsociada">Empresa Autora</label>
+          <input type="text" class="form-control" id="empresaAsociada" placeholder="CasGroup">
+        </div>
+        <div class="form-group">
+          <label for="bases">Bases</label>
+          <textarea class="form-control" id="bases" style="height: 150px;"></textarea>
+          <small id="basesHelpBlock" class="form-text text-muted">
+            Descripción de bases pendiente
+          </small>
+        </div>
+        <button type="submit" class="btn btn-primary">Crear</button>
+      </form>
     </div>
-    <div class="form-group">
-      <label for="exampleInputPassword1">Password</label>
-      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-    </div>
-    <div class="form-group form-check">
-      <input type="checkbox" class="form-check-input" id="exampleCheck1">
-      <label class="form-check-label" for="exampleCheck1">Check me out</label>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
+  </div>
 </template>
 
 <script>
@@ -23,6 +29,5 @@
   }
 </script>
 
-<style scoped>
-
+<style>
 </style>
