e007f1db8f033ce591d8228881534ebc297ee09e
gyahu <tpvchile@gmail.com>
2018-04-19 15:14:16 -0300

    Adds calendar choices for licitation creation

--- src/components/Dashboard/Views/Licitaciones/Nueva/Home.vue
+++ src/components/Dashboard/Views/Licitaciones/Nueva/Home.vue
@@ -17,6 +17,32 @@
             Descripción de bases pendiente
           </small>
         </div>
+        <div class="form-group">
+          <table>
+            <td v-for="periodo in periodos">
+              <label :for="periodo.label">{{periodo.title}}</label>
+              <div>
+                <div class="datepicker-trigger">
+                  <input
+                    type="text"
+                    :id="periodo.id"
+                    :placeholder="periodo.placeholder"
+                    :value="formatDates(periodo.dateOne, periodo.dateTwo)"
+                  >
+                  <AirbnbStyleDatepicker
+                    :trigger-element-id="periodo.id"
+                    :mode="'range'"
+                    :fullscreen-mobile="true"
+                    :date-one="periodo.dateOne"
+                    :date-two="periodo.dateTwo"
+                    @date-one-selected="val => { periodo.dateOne = val }"
+                    @date-two-selected="val => { periodo.dateTwo = val }"
+                  />
+                </div>
+              </div>
+            </td>
+          </table>
+        </div>
         <button type="submit" class="btn btn-primary">Crear</button>
       </form>
     </div>
@@ -24,8 +50,52 @@
 </template>
 
 <script>
+  import format from 'date-fns/format'
+
   export default {
-    name: 'Home.vue'
+    data() {
+      return {
+        dateFormat: 'D MMM',
+        periodos:{
+          primer_periodo: {
+            title: 'Primer Período',
+            label: 'periodo1',
+            placeholder: "Selecciona duración del primer Periodo",
+            dateOne: '',
+            dateTwo: '',
+            id: 'datepicker-trigger1'
+          },
+          segundo_periodo: {
+            title: 'Segundo Período',
+            label: 'periodo2',
+            placeholder: "Selecciona duración del segundo Periodo",
+            dateOne: '',
+            dateTwo: '',
+            id: 'datepicker-trigger2'
+          },
+          tercer_periodo: {
+            title: 'Tercer Período',
+            label: 'periodo3',
+            placeholder: "Selecciona duración del tercer Periodo",
+            dateOne: '',
+            dateTwo: '',
+            id: 'datepicker-trigger3'
+          }
+        }
+      }
+    },
+    methods: {
+      formatDates(dateOne, dateTwo) {
+        let formattedDates = ''
+        if (dateOne) {
+          formattedDates = format(dateOne, this.dateFormat)
+        }
+        if (dateTwo) {
+          formattedDates += ' - ' + format(dateTwo, this.dateFormat)
+        }
+        return formattedDates
+      }
+    }
   }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import LightBootstrap from './plugins/light-bootstrap-dashboard'
 import router from './router'
+import AirbnbStyleDatepicker from 'vue-airbnb-style-datepicker'
+import 'vue-airbnb-style-datepicker/dist/styles.css'
 
+// see docs for available options
+const datepickerOptions = {}
+
+// make sure we can use it in our components
+Vue.use(AirbnbStyleDatepicker, datepickerOptions)
 Vue.use(LightBootstrap)
 
 /* eslint-disable no-new */
