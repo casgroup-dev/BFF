8106f569e393c0646ba0e3e65a84d8aa5be0d161
Fabián Souto <fab.souto@gmail.com>
2018-05-17 14:52:08 -0400

    Add functionality to upload files to FileInputCard. Is ready now.

--- src/components/UIComponents/Inputs/FileInputCard.vue
+++ src/components/UIComponents/Inputs/FileInputCard.vue
@@ -5,12 +5,12 @@
       <h2 class="title" :style="{fontSize: titleSize}">{{ title }}</h2>
       <div class="custom-file">
         <input type="file" class="custom-file-input" :multiple="multiple"
-               ref="input"
-               @change="setFileNames">
+               ref="input" @change="setFiles">
         <label class="custom-file-label">{{inputLabelComputed}}</label>
       </div>
       <clip-loader :loading="loading" color="#1DC7EA" class="clip-loader"/>
-      <button v-if="!loading" class="btn btn-fill btn-round btn-upload" :style="{backgroundColor: buttonColor}"
+      <button v-if="!loading && files.length" class="btn btn-fill btn-round btn-upload"
+              :style="{backgroundColor: buttonColor}"
               @click="uploadFiles">
         <i class="fa fa-upload"></i> Subir archivos
       </button>
@@ -20,6 +20,8 @@
 
 <script>
   import ClipLoader from 'vue-spinner/src/ClipLoader'
+  import api from '../../../apis/users'
+  import axios from 'axios'
 
   const defaultColor = '#03A9F4'
 
@@ -36,8 +38,13 @@
      */
     data () {
       return {
-        fileNames: [],
-        loading: false
+        loading: false,
+        files: [],
+        baseNotification: {
+          horizontalAlign: 'right',
+          verticalAlign: 'bottom',
+          timeout: 20000
+        }
       }
     },
     /**
@@ -107,6 +114,13 @@
         const baseMessage = 'Seleccionar archivo'
         if (!this.multiple) return baseMessage
         else return baseMessage + 's'
+      },
+      /**
+       * Array with the names of the files.
+       */
+      fileNames () {
+        if (this.files.length) return Array.prototype.map.call(this.files, file => file.name)
+        else return []
       }
     },
     /**
@@ -114,13 +128,71 @@
      */
     methods: {
       /**
-       * Set the fileNames data to show the names in the input.
+       * Set files array from the input.
        */
-      setFileNames () {
-        this.fileNames = Array.prototype.map.call(this.$refs.input.files, file => file.name)
+      setFiles () {
+        this.files = this.$refs.input.files
       },
+      /**
+       * Upload the files to S3 and emit an event with every object uploaded.
+       */
       uploadFiles () {
         this.loading = true
+        /* Create promises that resolves with each signed request and url for each file */
+        const promises = Array.prototype.map.call(
+          this.files, // Files is not an array, is a FileList, so we need to call the map function like this
+          file => api.getSignedUrlToPutObject(file.name, file.type)
+        )
+        /* Wait for all promises to resolve */
+        Promise.all(promises).then(async results => {
+          /* results is an array of object with 'signedRequest' and 'url' (where the file can be downloaded) */
+          for (let i = 0; i < results.length; i++) {
+            let file = this.files[i]
+            let signedRequest = results[i].signedRequest
+            let urlToDownloadTheFile = results[i].url
+            try {
+              await axios.put(signedRequest, file, {headers: {'Content-Type': file.type}})
+              this.emitUploadedFileEvent(file.name, urlToDownloadTheFile)
+            } catch (err) {
+              console.error(err)
+              this.notifyError(file.name)
+            }
+          }
+          /* Back to normality */
+          this.loading = false
+          this.files = []
+        })
+      },
+      /**
+       * Emits an event when a new file is uploaded. It gives the url with the event to download the new file.
+       */
+      emitUploadedFileEvent (fileName, url) {
+        this.notifySuccess(fileName)
+        this.$emit('uploaded', url)
+      },
+      /**
+       * Show a notification indicating that the file was uploaded successfully.
+       * @param {String} fileName - Name of the file that was uploaded successfully.
+       */
+      notifySuccess (fileName) {
+        this.$notify({
+          ...this.baseNotification,
+          component: {template: `<span>Fue subido con éxito el archivo <b>${fileName}</b></span>`},
+          icon: 'fa fa-check',
+          type: 'success'
+        })
+      },
+      /**
+       * Show a notification as a warning indicating the the given file does not uploaded correctly.
+       * @param {String} fileName - Name of the file that was not uploaded.
+       */
+      notifyError (fileName) {
+        this.$notify({
+          ...this.baseNotification,
+          component: {template: `<span>Ocurrió un problema subiendo el archivo <b>${fileName}</b><br>Inténtalo nuevamente.</span>`},
+          icon: 'fa fa-exclamation',
+          type: 'warning'
+        })
       }
     }
   }
