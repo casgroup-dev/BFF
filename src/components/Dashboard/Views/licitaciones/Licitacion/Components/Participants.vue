b2db9ec1f237aaa04d37730b005e237cdb2ecb03
zerli <zerlink44@gmail.com>
2018-05-18 18:20:22 -0400

    Initial commit of participants component.

---
+++ src/components/Dashboard/Views/licitaciones/Licitacion/Components/Participants.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="participants-card">
+    <i v-if="icon" :class="`fa ${icon} icon`" :style="{color: iconColor}"></i>
+    <div class="content">
+      <h2 v-if="title" class="title" :style="{fontSize: titleSize}">{{ title }}</h2>
+      <div class=""
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Participants"
+    }
+</script>
+
+<style scoped>
+
+</style>
