2eea6fc5f8c16ea28173c421129ecf3a52036e23
zerli <zerlink44@gmail.com>
2018-05-23 17:26:06 -0400

    Modifies participants component.

--- src/components/Dashboard/Views/licitaciones/Licitacion/Components/Participants.vue
+++ src/components/Dashboard/Views/licitaciones/Licitacion/Components/Participants.vue
@@ -1,17 +1,68 @@
 <template>
-  <div class="participants-card">
-    <i v-if="icon" :class="`fa ${icon} icon`" :style="{color: iconColor}"></i>
-    <div class="content">
-      <h2 v-if="title" class="title" :style="{fontSize: titleSize}">{{ title }}</h2>
-      <div class=""
+  <stats-card>
+    <div slot="header" class="icon-success">
+      <i v-if="icon" :class="`fa ${icon} icon`" :style="{color: iconColor}"></i>
     </div>
-  </div>
+    <div slot="content" class="numbers">
+      <p>Participantes</p>{{ participants.length }}
+    </div>
+    <div slot="footer">
+      <button v-if="participants.length" class="btn btn-fill btn-round"
+              :style="{backgroundColor: buttonColor}">
+        <i class="fa fa-users"></i> Ver Participantes
+      </button>
+    </div>
+  </stats-card>
 </template>
 
 <script>
-    export default {
-        name: "Participants"
+  import StatsCard from 'src/components/UIComponents/Cards/StatsCard.vue'
+
+  const defaultColor = '#02B2A9'
+
+  export default {
+
+    components: {
+      StatsCard
+    },
+
+    data () {
+      return {
+        participants: [136689]
+      }
+    },
+
+    props: {
+      icon: {
+        type: String,
+        default: 'fa fa-users'
+      },
+      title: {
+        type: String,
+        default: 'Insert title here'
+      },
+      iconColor: {
+        type: String,
+        default: defaultColor
+      },
+      buttonColor: {
+        type: String,
+        default: defaultColor
+      },
+      titleSize: {
+        type: String,
+        default: '30px'
+      }
+    },
+
+    computed: {
+
+    },
+
+    methods: {
+
     }
+  }
 </script>
 
 <style scoped>
