0263b351da5f5f7d58eeeb79858df811769cc16c
martinpanza <martin.panza.p@gmail.com>
2018-06-13 20:57:42 -0400

    Bidding route id param

--- src/router/routes/index.js
+++ src/router/routes/index.js
@@ -37,7 +37,7 @@ const routes = [
         component: LicitacionesHome
       },
       {
-        path: '/Licitacion',
+        path: '/Licitacion/:id',
         name: 'licitacion',
         component: LicitacionLayout,
         props: true
